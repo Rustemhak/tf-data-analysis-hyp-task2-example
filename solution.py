512fb83fe4555b20ad597278d38ec5e08a27fb21
Rustem <61783564+Rustemhak@users.noreply.github.com>
2023-04-08 22:04:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import anderson_ksamp
 
 chat_id = 326525586 # Ваш chat ID, не меняйте название переменной
 
@@ -8,6 +8,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, p_value = mannwhitneyu(x, y)
+    p_value = anderson_ksamp([x, y]).pvalue 
     alpha = 0.07
     return p_value < alpha # Ваш ответ, True или False
