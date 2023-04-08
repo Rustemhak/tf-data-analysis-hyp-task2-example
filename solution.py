035d7dccf435db2d7c12e618bfe805e688f32310
Rustem <61783564+Rustemhak@users.noreply.github.com>
2023-04-08 18:23:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,6 +8,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, p_value = mannwhitneyu(x, y, alternative="two-sided")
+    _, p_value = mannwhitneyu(x, y)
     alpha = 0.07
     return p_value < alpha # Ваш ответ, True или False
